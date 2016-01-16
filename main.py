1dc7dacb4edfb247b637e758fa3f46adb12f499e
vyakhorev <Alexey.Vyakhorev@gmail.com>
2016-01-16 18:02:10 +0300

    Removed some comments

--- main.py
+++ main.py
@@ -2,19 +2,9 @@ from data_requester import cDataRequester
 from telbot import cTelegramServer
 from scheduler import cScheduler
 
-import threading
-
 ######################################
 # MAIN ###############################
 ######################################
-
-    # ######
-    # sch = cSchedEnv()
-    # sch.set_telegram_job_queue(job_queue)
-    # sch.add_task_telegram_reminder("reminder from global scheduler")
-    # sch.set_chats(db.get_all_chats(db_handler.get_active_session()))
-    # ######
-
 
 if __name__ == '__main__':
     # Init main data structures
@@ -26,9 +16,6 @@ if __name__ == '__main__':
     sched.start()
     sched.start_main_schedule()
     telegram_server.start()
-
-    #t_telegram = threading.Thread(target=)
-    #t_telegram.start()
 
 
 
